f070b46654cdf8066d792b28bd808c92fb54519f
Neviim <neviimdev@gmail.com>
2018-05-16 23:37:56 -0300

    Install elasticsearch e kabana
    pre-instalacao

---
+++ home/elasticsearch_install_mac.py
@@ -0,0 +1,32 @@
+# install elasticsearch mac
+
+	:: dependencias
+
+		- Instalar java 1.8 
+
+
+
+	:: instalando elasticsearch:
+
+		$ brew install nginx
+
+		$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+		$ brew install elasticsearch
+
+		$ brew install kibana
+
+
+
+
+	:: usando:
+
+		$ elasticsearch
+
+		- http://locallhost:9200
+
+
+
+
+# referencias
+	http://blog.abraseucodigo.com.br/melhorando-seus-logs-com-elk.html
